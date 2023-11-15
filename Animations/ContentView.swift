d135a511a8e97f178d61f0e5ee530c5aba4c0175
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-15 17:59:58 +0200

    Version 1.0

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    @State private var animation = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        Spacer()
+//        Button("Tap me") {
+////            animationAmount += 1
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundStyle(.white)
+//        .clipShape(.circle)
+////        .scaleEffect(animationAmount)
+////        .blur(radius: (animationAmount - 1) * 3)
+//        .overlay(
+//            Circle()
+//                .stroke(.red)
+//                .scaleEffect(animationAmount)
+//                .opacity(2 - animationAmount)
+//                .animation(
+//                    .easeOut(duration: 1)
+//                        .repeatForever(autoreverses: false),
+//                    value: animationAmount
+//                )
+//        )
+//        .onAppear{
+//            animationAmount = 2
+//        }
+//        Spacer()
+//        Spacer()
+        
+//        print(animation)
+//        
+//        return VStack {
+//            Stepper("Scale amount", value: $animation.animation(
+//                .easeInOut(duration: 1)
+//                    .repeatCount(3, autoreverses: true)
+//            ), in: 1...10)
+//            
+//            Spacer()
+//            
+//            Button("Tap Me") {
+//                animation += 1
+//            }
+//            .padding(40)
+//            .background(.red)
+//            .foregroundStyle(.white)
+//            .clipShape(.circle)
+//            .scaleEffect(animation)
+//        }
+        
+        Button("Tap Me") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)){
+                animationAmount += 360
+            }
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
     }
 }
 
