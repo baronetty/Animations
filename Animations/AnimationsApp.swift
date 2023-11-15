e317c7f2055607f3419ca662a6b8a3c68aa4af83
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-15 16:55:27 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Leo  on 15.11.23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Leo  on 15.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
