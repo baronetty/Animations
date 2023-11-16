1f2e18714690cb1e53c1beed4b29741e794003db
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-16 17:12:07 +0200

    Version 2.0

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading), 
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 struct ContentView: View {
     @State private var animationAmount = 0.0
     @State private var animation = 1.0
+    @State private var isShowingRed = false
+    
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    let letters = Array("Hello SwiftUI")
     
     var body: some View {
 //        Spacer()
@@ -59,16 +86,99 @@ struct ContentView: View {
 //            .scaleEffect(animation)
 //        }
         
-        Button("Tap Me") {
-            withAnimation(.spring(duration: 1, bounce: 0.5)){
-                animationAmount += 360
+//        Button("Tap Me") {
+//            withAnimation(.spring(duration: 1, bounce: 0.5)){
+//                animationAmount += 360
+//            }
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundStyle(.white)
+//        .clipShape(.circle)
+//        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
+//        Button("Tap Me") {
+//            enabled.toggle()
+//        }
+//        .frame(width: 200, height: 200)
+//        .background(enabled ? .blue : .red)
+//        .foregroundStyle(.white)
+//        .animation(.default, value: enabled) // Reihenfolge matters
+//        .clipShape(.rect(cornerRadius: enabled ? 60 : 10))
+//        .animation(.spring(duration: 1, bounce: 0.9), value: enabled)
+        
+        
+        
+//        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(.rect(cornerRadius: 10))
+//            .offset(dragAmount)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { dragAmount = $0.translation}
+//                    .onEnded { _ in
+//                        withAnimation(.bouncy) {
+//                            dragAmount = .zero }
+//                    }
+//            )
+        
+//        HStack(spacing: 0) {
+//            ForEach(0..<letters.count, id: \.self) { num in
+//                Text(String(letters[num]))
+//                    .padding(5)
+//                    .font(.title)
+//                    .background(enabled ? .blue : .red)
+//                    .offset(dragAmount)
+//                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+//            }
+//        }
+//        .gesture(
+//        DragGesture()
+//            .onChanged { dragAmount = $0.translation }
+//            .onEnded { _ in
+//                dragAmount = .zero
+//                enabled.toggle()
+//            }
+//        )
+        
+        
+        
+//        VStack{
+//            Button("Tap Me") {
+//                withAnimation {
+//                    isShowingRed.toggle()
+//                }
+//            }
+//            
+//            if isShowingRed {
+//                Rectangle()
+//                    .fill(.red)
+//                    .frame(width: 200, height: 200)
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//            }
+//        }
+        
+        
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+        
+        
+        
         
     }
 }
